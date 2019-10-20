197f1162fc8d48b283d6ef7a25f527bb731265f7
Timur Loghin <43240286+beater13@users.noreply.github.com>
2019-10-20 20:52:18 +0300

    Delete Main.java

--- Lab 2/Box/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Box box = new Box(1, 2 ,3);
-
-        System.out.println(box.calculateSurface());
-        System.out.println(box.calculateVolume());
-    }
-}
